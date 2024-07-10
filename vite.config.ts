69a0cb11be6081baca83ccbaac6a748649ac947e
183600 <zhongwenjia2009@163.com>
2024-07-10 00:43:34 +0000

    删除文件 vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineApplicationConfig } from './build/vite/application'
-
-export default defineApplicationConfig({
-  overrides: {
-    optimizeDeps: {
-      include: ['echarts/core', 'echarts/charts', 'echarts/components', 'echarts/renderers', '@iconify/iconify', '@element-plus/icons-vue'],
-    },
-  },
-})

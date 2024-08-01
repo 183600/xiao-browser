e30eaa53d43a90931422c3246d865e223eec844a
183600 <zhongwenjia2009@163.com>
2024-08-01 09:06:20 +0800

    实现基本功能

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -1,92 +1,70 @@
-    // 找到要添加点击事件的按钮元素
-    var button = document.getElementById("gitee");
+// 找到要添加点击事件的按钮元素
+var button = document.getElementById("gitee");
 
-    // 添加点击事件监听器
-    button.addEventListener("click", function() {
-        // 在点击事件发生后访问链接
-        window.location.href = "https://gitee.com/qwe12345678";
-        window.location.href = "https://gitee.com/qwe12345678";
-    });
+// 添加点击事件监听器
+button.addEventListener("click", function () {
+    // 在点击事件发生后访问链接
+    window.location.href = "https://gitee.com/qwe12345678";
+    window.location.href = "https://gitee.com/qwe12345678";
+});
 
-    // 找到要添加点击事件的按钮元素
-    var button = document.getElementById("gitcode");
+// 找到要添加点击事件的按钮元素
+var button = document.getElementById("github");
 
-    // 添加点击事件监听器
-    button.addEventListener("click", function() {
-        // 在点击事件发生后访问链接
-        window.location.href = "https://gitcode.com/m0_49178139";
-        window.location.href = "https://gitcode.com/m0_49178139";
-    });
+// 添加点击事件监听器
+button.addEventListener("click", function () {
+    // 在点击事件发生后访问链接
+    window.location.href = "https://github.com/183600";
+    window.location.href = "https://github.com/183600";
+});
 
-    // 找到要添加点击事件的按钮元素
-    var button = document.getElementById("kuan");
+// 找到要添加点击事件的按钮元素
+var button = document.getElementById("kuan");
 
-    // 添加点击事件监听器
-    button.addEventListener("click", function() {
-        // 在点击事件发生后访问链接
-        window.location.href = "https://www.coolapk.com/u/1910517";
-        window.location.href = "https://www.coolapk.com/u/1910517";
-    });
+// 添加点击事件监听器
+button.addEventListener("click", function () {
+    // 在点击事件发生后访问链接
+    window.location.href = "https://www.coolapk.com/u/1910517";
+    window.location.href = "https://www.coolapk.com/u/1910517";
+});
 
-    // 找到要添加点击事件的按钮元素
-    var button = document.getElementById("yuanmagitee");
+// 找到要添加点击事件的按钮元素
+var button = document.getElementById("yuanmagitee");
 
-    // 添加点击事件监听器
-    button.addEventListener("click", function() {
-        // 在点击事件发生后访问链接
-        window.location.href = "https://gitee.com/qwe12345678/web-page-analysis";
-        window.location.href = "https://gitee.com/qwe12345678/web-page-analysis";
-    });
-
-
-    // 找到要添加点击事件的按钮元素
-    var button = document.getElementById("yuanmagitcode");
-
-    // 添加点击事件监听器
-    button.addEventListener("click", function() {
-        // 在点击事件发生后访问链接
-        window.location.href = "https://gitcode.com/m0_49178139/web-page-analysis/overview";
-        window.location.href = "https://gitcode.com/m0_49178139/web-page-analysis/overview";
-    });
-
-    // 找到要添加点击事件的按钮元素
-    var button = document.getElementById("qun");
-
-    // 添加点击事件监听器
-    button.addEventListener("click", function() {
-        // 在点击事件发生后访问链接
-        window.location.href = "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=UbtLs1ReDi94nOH-CVfcXwMuNblBeiYD&authKey=%2BuJcSuT9YZ3z%2Faf7mCxE1W5KBiC7xDJUDZ1JR8qqqQ2Tlv7GSCoo98qX8I6%2B4g11&noverify=0&group_code=854260276";
-        window.location.href = "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=UbtLs1ReDi94nOH-CVfcXwMuNblBeiYD&authKey=%2BuJcSuT9YZ3z%2Faf7mCxE1W5KBiC7xDJUDZ1JR8qqqQ2Tlv7GSCoo98qX8I6%2B4g11&noverify=0&group_code=854260276";
-    });
-
-    // 找到要添加点击事件的按钮元素
-    var button = document.getElementById("yuanmagiteefirefoxban");
-
-    // 添加点击事件监听器
-    button.addEventListener("click", function() {
-        // 在点击事件发生后访问链接
-        window.location.href = "https://gitee.com/qwe12345678/web-page-analysis_firefoxban";
-        window.location.href = "https://gitee.com/qwe12345678/web-page-analysis_firefoxban";
-    });
+// 添加点击事件监听器
+button.addEventListener("click", function () {
+    // 在点击事件发生后访问链接
+    window.location.href = "https://gitee.com/qwe12345678/mini-browser";
+    window.location.href = "https://gitee.com/qwe12345678/mini-browser";
+});
 
 
-    // 找到要添加点击事件的按钮元素
-    var button = document.getElementById("yuanmagitcodefirefoxban");
+// 找到要添加点击事件的按钮元素
+var button = document.getElementById("yuanmagithub");
 
-    // 添加点击事件监听器
-    button.addEventListener("click", function() {
-        // 在点击事件发生后访问链接
-        window.location.href = "https://gitcode.com/m0_49178139/web-page-analysis_firefoxban";
-        window.location.href = "https://gitcode.com/m0_49178139/web-page-analysis_firefoxban";
-    });
-var lista = "的,和,中,在,或,是"
-  // 获取输入值
-  var list = lista.split(",");
-  // 保存设置
-  chrome.storage.sync.set({ list: list }, function() {
-  });
-  // 保存设置
-  chrome.storage.sync.set({ switch: true }, function() {
-  });
+// 添加点击事件监听器
+button.addEventListener("click", function () {
+    // 在点击事件发生后访问链接
+    window.location.href = "https://github.com/183600/xiao-browser";
+    window.location.href = "https://github.com/183600/xiao-browser";
+});
 
+// 找到要添加点击事件的按钮元素
+var button = document.getElementById("qun");
 
+// 添加点击事件监听器
+button.addEventListener("click", function () {
+    // 在点击事件发生后访问链接
+    window.location.href = "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=WbDabgCtzAThSJxKavefPPZVO3kRhgQZ&authKey=zWZdBDbcfHfUrBhEGwmmCyd3%2FAiyzuqIPDL121CUukxdPMjJl37fWsuptbW1A69o&noverify=0&group_code=970971601";
+    window.location.href = "http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=WbDabgCtzAThSJxKavefPPZVO3kRhgQZ&authKey=zWZdBDbcfHfUrBhEGwmmCyd3%2FAiyzuqIPDL121CUukxdPMjJl37fWsuptbW1A69o&noverify=0&group_code=970971601";
+});
+
+// 找到要添加点击事件的按钮元素
+var button = document.getElementById("fangwenguanwang");
+
+// 添加点击事件监听器
+button.addEventListener("click", function () {
+    // 在点击事件发生后访问链接
+    window.location.href = "https://minibrowser7.wordpress.com/";
+    window.location.href = "https://minibrowser7.wordpress.com/";
+});
